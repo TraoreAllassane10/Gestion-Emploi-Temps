b6d2331b496c09a0cb1e34f4895a537505f9a7a9
Traore Allassane <traoreallassane2255@gmail.com>
2025-11-17 08:09:54 +0000

    Setup

---
+++ resources/js/components/app-sidebar.tsx
@@ -0,0 +1,95 @@
+import { NavFooter } from '@/components/nav-footer';
+import { NavMain } from '@/components/nav-main';
+import { NavUser } from '@/components/nav-user';
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from '@/components/ui/sidebar';
+import { dashboard } from '@/routes';
+import { type NavItem } from '@/types';
+import { Link } from '@inertiajs/react';
+import { Book, BookA, BookOpen, Folder, House, LayoutGrid, LucideMoveUpLeft, User } from 'lucide-react';
+import AppLogo from './app-logo';
+
+const mainNavItems: NavItem[] = [
+    {
+        title: 'Dashboard',
+        href: dashboard(),
+        icon: LayoutGrid,
+    },
+    {
+        title: 'Annee Scolaire',
+        href: dashboard(),
+        icon: Book,
+    },
+    {
+        title: 'Niveau',
+        href: dashboard(),
+        icon: LucideMoveUpLeft,
+    },
+    {
+        title: 'Filière',
+        href: dashboard(),
+        icon: BookOpen,
+    },
+    {
+        title: 'Professeur',
+        href: dashboard(),
+        icon: User,
+    },
+      {
+        title: 'Salle',
+        href: dashboard(),
+        icon: House,
+    },
+      {
+        title: 'Cours',
+        href: dashboard(),
+        icon: BookA
+    },
+];
+
+const footerNavItems: NavItem[] = [
+    {
+        title: 'Repository',
+        href: 'https://github.com/laravel/react-starter-kit',
+        icon: Folder,
+    },
+    {
+        title: 'Documentation',
+        href: 'https://laravel.com/docs/starter-kits#react',
+        icon: BookOpen,
+    },
+];
+
+export function AppSidebar() {
+    return (
+        <Sidebar collapsible="icon" variant="inset">
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton size="lg" asChild>
+                            <Link href={dashboard()} prefetch>
+                                <AppLogo />
+                            </Link>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+
+            <SidebarContent>
+                <NavMain items={mainNavItems} />
+            </SidebarContent>
+
+            <SidebarFooter>
+                <NavFooter items={footerNavItems} className="mt-auto" />
+                <NavUser />
+            </SidebarFooter>
+        </Sidebar>
+    );
+}
