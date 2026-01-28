7d665d92fb638679c957fbf230cf4398433c4d42
Traore Allassane <traoreallassane2255@gmail.com>
2026-01-28 13:20:15 +0000

    feat : Creation des migrations pour la gestion des utilisateurs

---
+++ database/migrations/2026_01_28_130643_create_etudiants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            // Identifiant permanent de l'etudiant
+            $table->string("ip")->primary();
+            
+            $table->string("nom");
+            $table->string("prenom");
+            $table->date("date_naissane");
+            $table->string("lieu_naissane");
+            $table->integer("numero");
+            $table->string("nom_parent");
+            $table->integer("numero_parent");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etudiants');
+    }
+};
