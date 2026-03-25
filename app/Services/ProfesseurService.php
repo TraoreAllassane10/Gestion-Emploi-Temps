f1503809364a7d15f80351bf383fb482a360ace3
Traore Allassane <traoreallassane2255@gmail.com>
2026-03-25 13:08:31 +0000

    refactor(Professeur) : Factorisation de la logique avec le service Pattern

---
+++ app/Services/ProfesseurService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Professeur;
+use App\Repositories\ProfesseurRepository;
+
+class ProfesseurService
+{
+
+    public function __construct(
+        protected ProfesseurRepository $professeurRepository
+    ){}
+
+    public function getAllProfesseurs() {
+        return $this->professeurRepository->all();
+    }
+
+    public function createProfesseur(array $data){
+        return $this->professeurRepository->create($data);
+    }
+
+     public function updateProfesseur(Professeur $professeur, array $data){
+        return $this->professeurRepository->update($professeur, $data);
+    }
+
+     public function deleteProfesseur(Professeur $professeur){
+        return $this->professeurRepository->delete($professeur);
+    }
+}
