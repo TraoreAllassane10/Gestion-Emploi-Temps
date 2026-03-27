9d6f048e139558f49b230b22018e7eb1d11ec71d
Traore Allassane <traoreallassane2255@gmail.com>
2026-03-27 16:17:04 +0000

    test(Inscription) : Debut de la redaction du test d'inscription

--- tests/Feature/InscriptionTest.php
+++ tests/Feature/InscriptionTest.php
@@ -10,23 +10,23 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 uses(RefreshDatabase::class);
 
-// test('Un utilisateur peut effectuer une inscription', function () {
-//     $user = User::factory()->create();
-//     $this->actingAs($user);
+test('Un utilisateur peut effectuer une inscription', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
 
-//     $etudiant = Etudiant::factory()->create();
-//     $annee = AnneeUniversitaire::factory()->active()->create();
-//     $niveau = Niveau::factory()->withScolarite()->create();
-//     $frais = FraisConfiguration::factory()->create();
+    $etudiant = Etudiant::factory()->create();
+    $annee = AnneeUniversitaire::factory()->active()->create();
+    $niveau = Niveau::factory()->withScolarite()->create();
+    $frais = FraisConfiguration::factory()->create();
 
-//     $response = $this->post("/inscriptions/create", [
-//         "taux_reduction" => 0,
-//         "type_inscription" => TypeInscription::NOUVELLE->value,
-//         "etudiant_ip" => $etudiant->ip,
-//         "annee_id" => $annee->id,
-//         "niveaux" => $niveau->id
-//     ]);
+    $response = $this->post("/inscriptions/create", [
+        "taux_reduction" => 0,
+        "type_inscription" => TypeInscription::NOUVELLE->value,
+        "etudiant_ip" => $etudiant->ip,
+        "annee_id" => $annee->id,
+        "niveaux" => $niveau->id
+    ]);
 
-//     // $response->assertStatus(200);
-//     $this->assertDatabaseCount("inscriptions", 1);
-// });
+    // $response->assertStatus(200);
+    $this->assertDatabaseCount("inscriptions", 1);
+});
